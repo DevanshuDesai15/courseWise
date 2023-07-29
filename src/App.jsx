c32d853be5e60fa540234056d2d7362be00ea1bb
DevanshuDesai15 <devanshudesai_1509@yahoo.com>
2023-07-29 14:28:19 -0700

    code: installed necessary packages and updated app

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './components/Login';
+import Landing from "./components/Landing";
+import CreateCourse from './components/CreateCourse';
+import Register from './components/Register';
+import ShowCourses from './components/ShowCourses';
+import { createTheme, ThemeProvider} from '@mui/material/styles';
+import Header from './sub-components/Header';
+import Course from './components/Course';
+import { RecoilRoot } from 'recoil';
+
+
+const theme = createTheme({
+    typography: {
+        fontFamily: 'Ubuntu',
+    }
+});
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <ThemeProvider theme={theme}>
+            <RecoilRoot>
+                <Router>
+                    <Header/>
+                    <Routes>
+                        <Route path="/" element={<Landing />} />
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/register" element={<Register />} />
+                        <Route path="/createcourse" element={<CreateCourse />} />
+                        <Route path="/courses" element={<ShowCourses />} />
+                        <Route path="/course/:courseId" element={<Course />} />
+                    </Routes>
+                </Router>
+            </RecoilRoot>
+        </ThemeProvider>
+    );
 }
 
-export default App
+export default App;
