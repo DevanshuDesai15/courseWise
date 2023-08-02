0c1ace9738bc2e483beee86d829779ad449dcbfc
DevanshuDesai15 <devanshudesai_1509@yahoo.com>
2023-08-02 11:51:17 -0700

    fix: axios to fetch API used

--- src/components/Course.jsx
+++ src/components/Course.jsx
@@ -2,30 +2,23 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import ReactLoading from 'react-loading';
-import { Card, CardMedia, CardContent, Typography, TextField, Button } from "@mui/material";
+import { Card, Grid, CardMedia, CardContent, Typography, TextField, Button, FormControlLabel, Checkbox } from "@mui/material";
+import axios from "axios";
 
 function Course () {
     let { courseId } = useParams();
-    const [courses, setCourses] = useState([]);
+    const [course, setCourse] = useState(null);
 
     useEffect(() => {
-        fetch('http://localhost:3000/admin/courses/', {
-                                headers: {
-                                    "Authorization": "Bearer " + localStorage.getItem("token")
-                                }
-                            }).then((res) => {
-                                res.json().then((data) => {
-                                    setCourses(data.courses);
-                                })
-                            })
+        axios.get(`http://localhost:3000/admin/courses/${courseId}`, {
+            method: "GET",
+            headers: {
+                "Authorization": "Bearer " + localStorage.getItem("token")
+            }
+        }).then((res) => {
+            setCourse(res.data.course);
+        });
     }, [])
-
-    let course = null;
-    for (let i =0; i<courses.length; i++) {
-        if(courses[i].id == courseId) {
-            course = courses[i]
-        }
-    }
 
     if(!course) {
         return (
@@ -37,20 +30,39 @@ function Course () {
 
     return (
         <>
-            <CourseCard course = {course}/>
-            <UpdateCard courses = {courses} course = {course} setCourses = {setCourses} />
+            <EditTopper title={course.title}/>
+            <Grid container>
+                <Grid item lg={8} md={12} sm={12}>
+                    <UpdateCard course = {course} setCourse = {setCourse} />
+                </Grid>
+                <Grid item lg={4} md={12} sm={12}>
+                    <CourseCard course = {course}/>
+                </Grid>
+            </Grid>
         </>
     )
 }
 
-function CourseCard({ course }) {
-    const { title, description, price, image, published} = course ;
+function EditTopper({ title }) {
     return (
-        <div style={{ display: 'flex', justifyContent: "center", marginTop:20}}>
-            <Card sx={{ maxWidth: 345 }}>
+        <div style={{height: 250, background:"#04068d", top: 0, width: "100vw", zIndex: 0, marginBottom: -250}}>
+            <div style={{ height: 250, display: "flex", justifyContent:"center", flexDirection: "column"}}>
+                <div>
+                    <Typography style={{ color: "white", fontWeight: 600}} variant="h3" textAlign={"center"}>{title}</Typography>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+function CourseCard({ course }) {
+    const { title, description, price, imageLink, published} = course ;
+    return (
+        <div style={{ display: 'flex', justifyContent: "center", marginTop:80}}>
+            <Card sx={{ width: 345 }}>
                 <CardMedia
-                sx={{ height: 140 }}
-                image={image}
+                sx={{ height: 200 }}
+                image={imageLink}
                 title={title}
                 />
                 <CardContent>
@@ -64,62 +76,50 @@ function CourseCard({ course }) {
     )
 }
 
-function UpdateCard({course, courses, setCourses}) {
-    const [title, setTitle] = useState("");
-    const [description, setDescription] = useState("");
-    const [price, setPrice] = useState("");
-    const [image, setImage] = useState("");
-    const [published, setPublished] = useState(false);
+function UpdateCard({course, setCourse}) {
+    const [title, setTitle] = useState(course.title);
+    const [description, setDescription] = useState(course.description);
+    const [price, setPrice] = useState(course.price);
+    const [image, setImage] = useState(course.imageLink);
+    const [checked, setChecked] = useState(course.published);
 
     return (
-        <div style={{ display: 'flex', justifyContent: "center", marginTop:20}}>
-            <Card sx={{ minWidth: 275 }} style={{width: 400, padding: 20}}>
-                <div>
-                    <TextField fullWidth={true} variant="outlined" label="Title" type={"text"} onChange={e => setTitle(e.target.value)} />
+        <div style={{ display: 'flex', justifyContent: "center"}}>
+            <Card variant={"outlined"} style={{width: 600, marginTop: 200}}>
+                <div style={{ padding: 20}}>
+                    <TextField fullWidth={true} value={title} variant="outlined" label="Title" type={"text"} onChange={e => setTitle(e.target.value)} />
                     <br /><br />
-                    <TextField fullWidth={true} variant="outlined" label="Description" type={"text"} onChange={e => setDescription(e.target.value)} />
+                    <TextField fullWidth={true} value={description} variant="outlined" label="Description" type={"text"} onChange={e => setDescription(e.target.value)} />
                     <br /> <br />
-                    <TextField fullWidth={true} variant="outlined" label="Price" type={"number"} onChange={e => setPrice(e.target.value)} />
+                    <TextField fullWidth={true} value={price} variant="outlined" label="Price" type={"number"} onChange={e => setPrice(e.target.value)} />
                     <br /> <br />
-                    <TextField fullWidth={true} variant="outlined" label="Image Link" type={"url"} onChange={e => setImage(e.target.value)}/>
+                    <TextField fullWidth={true} value={image} variant="outlined" label="Image Link" type={"url"} onChange={e => setImage(e.target.value)}/>
                     <br /> <br />
-                    <TextField fullWidth={true} variant="outlined" label="Published" type={""} onChange={e => setPublished(e.target.value)}/>
+                    <FormControlLabel label="Publish" control={<Checkbox checked={checked} onChange={e => setChecked(e.target.checked)}/>} inputProps={{ 'aria-label': 'controlled' }}/> 
                     <br /> <br />
                     <div style={{display:'flex', justifyContent:'space-between'}}>
-                        <Button variant="contained" onClick={() => {
-                            fetch("http://localhost:3000/admin/courses/" + course.id, {
-                                method: "PUT",
-                                body: JSON.stringify({
+                        <Button variant="contained" onClick={ async () => {
+                            axios.put("http://localhost:3000/admin/courses/" + course._id, {
                                     title,
                                     description,
                                     price,
-                                    image,
-                                    published
-                                }),
+                                    imageLink: image,
+                                    published: checked
+                                }, {
                                 headers: {
                                     "Content-type": "application/json",
                                     "Authorization": "Bearer " + localStorage.getItem("token")
                                 }
-                            }).then((res) => {
-                                res.json().then(() => {
-                                    let updateCourses = [];
-                                    for (let i=0; i<courses.length; i++){
-                                        if( courses[i].id == course.id) {
-                                            updateCourses.push({
-                                                id: course.id,
-                                                title,
-                                                description,
-                                                price,
-                                                image,
-                                                published
-                                            })
-                                        } else {
-                                            updateCourses.push(courses[i]);
-                                        }
-                                    }
-                                    setCourses(updateCourses);
-                                })
                             })
+                            let UpdateCourse = {
+                                _id: course._id,
+                                title: title,
+                                description: description,
+                                imageLink: image,
+                                price,
+                                published: checked
+                            };
+                            setCourse(UpdateCourse);
                         }}>
                             Update
                         </Button>
