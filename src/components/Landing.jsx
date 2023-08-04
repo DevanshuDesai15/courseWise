6319b218c3b77319317d4562712e5ecc521cbb9e
DevanshuDesai15 <devanshudesai_1509@yahoo.com>
2023-08-03 23:18:12 -0700

    feat: image added in landing page

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -5,11 +5,12 @@ import { Typography } from "@mui/material";
 /// Logging a user out is as simple as deleting the token from the local storage.
 function Landing() {
     return (
-        <div style={{ display: 'flex', justifyContent:'space-between', marginTop: 100}} >
-            <Typography variant="h5"><b> Learn from industry-leading professionals and gain hands-on expertise in the most in-demand technologies. </b></Typography>
-            <div>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam illo doloremque tempora cum eligendi non iusto dolore! Vero quisqua
-                <Typography variant="h5">IMGAE COMING SOON!!</Typography>
+        <div style={{ display: 'flex', justifyContent:'space-between', marginTop: 90}} >
+            <div style={{padding: 20}}>
+                <Typography variant="h5"><b> Learn from industry-leading professionals and gain hands-on expertise in the most in-demand technologies. </b></Typography>
+            </div>
+            <div style={{padding: 20}}>
+                <img src="https://media.istockphoto.com/id/1396696283/photo/teacher-with-students-in-computer-lab.jpg?s=612x612&w=0&k=20&c=UTxIcsUZ2AGeuMXRaPOpGSYeP1caHtTanMIpOlC4DW8=" alt="Instructor Teaching" />
             </div>
         </div>
     )
